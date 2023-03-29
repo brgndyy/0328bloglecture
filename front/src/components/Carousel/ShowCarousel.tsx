c7cb9209ec52319e5c3dddb2d744de2ad13a68e5
brgndy <brgndy96@gmail.com>
2023-03-29 15:49:33 +0900

    msg

---
+++ front/src/components/Carousel/ShowCarousel.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import classes from "./ShowCarousel.module.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { postsType } from "../../../types/postsType";
+import PostItem from "../FeaturedPosts/PostItem";
+
+type CarouselPostType = {
+  carouselPosts: postsType[];
+};
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 3, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
+
+export default function ShowCarousel({ carouselPosts }: CarouselPostType) {
+  return (
+    <>
+      <h1 className={classes.logo}>You May Like</h1>
+      <div className={classes.carousel_container}>
+        <Carousel
+          responsive={responsive}
+          ssr={true}
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={3000}
+          draggable={false}
+          arrows={false}
+        >
+          {carouselPosts.map((post) => {
+            return (
+              <PostItem
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                description={post.description}
+                date={post.date}
+                category={post.category}
+                path={post.path}
+                featured={post.featured}
+                imgUrl={post.imgUrl}
+              />
+            );
+          })}
+        </Carousel>
+      </div>
+    </>
+  );
+}
